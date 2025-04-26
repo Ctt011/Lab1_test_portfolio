e80769a535be78e1629d2dde68bc61560abafebe
Camille Tran <Ctt011@ucsd.edu>
2025-04-25 23:17:05 -0700

    lab4

---
+++ index.js
@@ -0,0 +1,23 @@
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
+
+// Fetch project data and display the latest 3
+const projects = await fetchJSON('./lib/projects.json');
+const latestProjects = projects.slice(0, 3);
+
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
+
+// Fetch GitHub stats and display them
+const githubData = await fetchGitHubData('Ctt011');  // 👈 your GitHub username
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
